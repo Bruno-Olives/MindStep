88ad406fe2b6bb2677e211f11191d49e4d3a0584
Bruno-Olives <brunobroliv05@gmail.com>
2026-03-31 21:31:11 +0100

    feat: list entries

--- app/src/main/java/com/example/mindstep/data/local/EntryDao.kt
+++ app/src/main/java/com/example/mindstep/data/local/EntryDao.kt
@@ -3,10 +3,15 @@ package com.example.mindstep.data.local
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface EntryDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(entry: EntryEntity)
+
+    @Query("SELECT * FROM entries ORDER BY id DESC")
+    fun getAllEntries(): Flow<List<EntryEntity>>
 }
 
