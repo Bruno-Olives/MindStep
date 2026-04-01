2c988702947307c54267e02da071666bd4fbc7d2
Bruno-Olives <brunobroliv05@gmail.com>
2026-04-01 21:42:12 +0100

    feat: labels

---
+++ app/src/main/java/com/example/mindstep/utils/labels.kt
@@ -0,0 +1,6 @@
+package com.example.mindstep.utils
+
+
+val moodLabels = listOf("Muito mal", "Mal", "Neutro", "Bem", "Muito bem")
+
+val anxietyLabels = listOf("Muito baixa", "Baixa", "Moderada", "Alta", "Muito alta")
